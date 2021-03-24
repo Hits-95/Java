0735bfb2465791ed5437189e8e9abcf8438a10e7
Ahire Hitesh <hbahire2014@gmail.com>
2021-03-24 23:52:05 +0530

    add New Data

--- Maven/springDemo/src/main/java/com/springDemo/springDemo/App.java
+++ Maven/springDemo/src/main/java/com/springDemo/springDemo/App.java
@@ -8,7 +8,9 @@ public class App {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springDemo/springDemo/conf.xml");
 		Student student1 = (Student) context.getBean("Student1");
 		Student student2 = (Student) context.getBean("Student2");
+		Student student3 = (Student) context.getBean("Student3");
 		System.out.println(student1);
 		System.out.println(student2);
+		System.out.println(student3);
 	}
 }
