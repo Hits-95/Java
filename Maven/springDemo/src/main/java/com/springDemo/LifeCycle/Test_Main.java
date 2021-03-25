1dab543d27a63e8497de211c77da35f57cd9a6ba
Ahire Hitesh <hbahire2014@gmail.com>
2021-03-25 23:26:57 +0530

    add

---
+++ Maven/springDemo/src/main/java/com/springDemo/LifeCycle/Test_Main.java
@@ -0,0 +1,18 @@
+package com.springDemo.LifeCycle;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test_Main {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/springDemo/LifeCycle/data_confg.xml");
+		Samosa samosa1 = (Samosa) context.getBean("samosa1");
+		System.out.println(samosa1);
+		
+		//registring shut down hook
+		context.registerShutdownHook();
+	}
+
+}
