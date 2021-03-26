9ee10ba6d087b2d5e45b151d74e4cb2de279c877
Ahire Hitesh <hbahire2014@gmail.com>
2021-03-26 12:39:58 +0530

    add

--- Spring/springDemo/src/main/java/com/springDemo/auto/wire/annotation/Employee.java
+++ Spring/springDemo/src/main/java/com/springDemo/auto/wire/annotation/Employee.java
@@ -1,16 +1,20 @@
 package com.springDemo.auto.wire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
+	
 	@Autowired
+	@Qualifier("address2")
 	private Address address;
 
 	public Employee() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	//@Autowired
 	public Employee(Address address) {
 		super();
 		this.address = address;
@@ -19,7 +23,8 @@ public class Employee {
 	public Address getAddress() {
 		return address;
 	}
-
+	
+	//@Autowired
 	public void setAddress(Address address) {
 		this.address = address;
 	}
