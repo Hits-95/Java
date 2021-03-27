5a8753ea5ba8cd34ed550a2b3c173ae3b7decb67
Ahire Hitesh <hbahire2014@gmail.com>
2021-03-27 14:22:04 +0530

    update

--- Spring/springDemo/src/main/java/com/springDemo/spEx/lang/Demo.java
+++ Spring/springDemo/src/main/java/com/springDemo/spEx/lang/Demo.java
@@ -5,15 +5,28 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Demo {
-	@Value("#{11 + 95}")
+	// create exprn
+	@Value("#{ 11 + 95 }")
 	private int x;
-	@Value("#{10 > 5 ? 11 : 44}")
+
+	@Value("#{ 10 > 5 ? 11 : 44 }")
 	private int y;
 
-	@Override
-	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
-	}
+	// calling method..
+	@Value("#{ T(java.lang.Math).sqrt(144) }")
+	private double z;
+
+	// calling variable value
+	@Value("#{ T(java.lang.Math).E }")
+	private double e;
+
+	// calling variable value
+	@Value("#{T(java.lang.Math).PI}")
+	private double pi;
+
+	// creating object and call its constructer
+	@Value("#{ new java.lang.String('Hitesh Ahire') }")
+	private String name;
 
 	public int getX() {
 		return x;
@@ -30,4 +43,42 @@ public class Demo {
 	public void setY(int y) {
 		this.y = y;
 	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+	public double getE() {
+		return e;
+	}
+
+	public void setE(double e) {
+		this.e = e;
+	}
+
+	public double getPi() {
+		return pi;
+	}
+
+	public void setPi(double pi) {
+		this.pi = pi;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", pi=" + pi + ", name=" + name + "]";
+	}
+
 }
