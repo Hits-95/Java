b5c3cf0deff924b126277b7e072905eeb414de57
Ahire Hitesh <hbahire2014@gmail.com>
2021-03-29 17:38:14 +0530

    update

--- Spring/HibernateDemo/src/main/java/com/hibernate/HibernateDemo/App.java
+++ Spring/HibernateDemo/src/main/java/com/hibernate/HibernateDemo/App.java
@@ -1,5 +1,7 @@
 package com.hibernate.HibernateDemo;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -15,12 +17,25 @@ public class App {
 		Student student = new Student(2, "Prashunya Shewale", "@Tehare, Malegaon, Nashi");
 		System.out.println(student);
 		
+		//creating object of Address
+		Address address = new Address();
+		address.setStreet("street1");
+		address.setCity("Nashik");
+		address.setOpen(false);
+		address.setAddedDate(new Date());
+		address.setBill(12313.12);
+		//address
+		
 		//Session session = factory.getCurrentSession();
 		Session session = factory.openSession();
 		Transaction tx = session.beginTransaction();
+		
 		session.save(student);
+		session.save(address);
 		tx.commit();
+		
 		session.close();
+		System.out.println("Done.....");
 		/*
 		 * System.out.println(factory); System.out.println(factory.isOpen());
 		 * System.out.println(factory.isClosed());
