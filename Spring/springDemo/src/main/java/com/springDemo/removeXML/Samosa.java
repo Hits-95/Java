ad798e0446f7c15007799d1e3f9f083089e50290
Ahire Hitesh <hbahire2014@gmail.com>
2021-03-31 13:36:57 +0530

    update

--- Spring/springDemo/src/main/java/com/springDemo/removeXML/Samosa.java
+++
@@ -1,8 +0,0 @@
-package com.springDemo.removeXML;
-
-public class Samosa {
-
-	public void display() {
-		System.out.println("Price is little be high...");
-	}
-}
--- Spring/springDemo/src/main/java/com/springDemo/stereotype/Tea.java
+++
@@ -1,5 +0,0 @@
-package com.springDemo.stereotype;
-
-public class Tea {
-
-}
